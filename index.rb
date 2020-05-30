72670d41dc06907089389b27237936825a908147
rafi993 <rafi993@protonmail.com>
2020-05-30 12:10:04 +0530

    Commandline help options

--- index.rb
+++ index.rb
@@ -5,4 +5,21 @@ require_relative './zepto'
 default_config_file = File.open "default_config.json"
 default_config = JSON.load default_config_file
 
-puts default_config
+case ARGV[0]
+when "serve"
+  puts "Starting server in port 3000"
+when "build"
+  puts "Building static files"
+when "help"
+  puts %(
+         zepto serve: To serve the files locally in dev mode
+         zepto build: Building for production
+         zepto help: Help options
+
+         For more details visit https://github.com/Rafi993/zepto
+        )
+else
+  puts "I don't understand the command. Try running 'zepto help'"
+end
+
+# puts default_config
